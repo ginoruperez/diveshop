69514e686c0adf8e191b1143a7485f6ea40c7a71
ginoruperez <ginoruperez@gmail.com>
2022-07-04 21:51:16 +0100

    Create AdvOpenWater.js

---
+++ src/components/AdvOpenWater.js
@@ -0,0 +1,242 @@
+import React from 'react';
+import dolphinico from './images/dolphin.ico';
+import advancedopen from './images/products/services-advancedopen.jpg';
+import mermaid from './images/products/mermaid-small.jpg';
+import homedolphin from './images/products/home-dolphin-small.jpg';
+import photography from './images/products/dive-photography.jpg';
+import boatdiver from './images/products/boatdiver.jpg';
+import dolphin3 from './images/dolphin3.png';
+
+
+function AdvOpenWater() {
+
+    return (
+        <div>
+            <header>
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
+                <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+                    <div className="container">
+                        <a className="navbar-brand" href="/">
+                            <img src={dolphinico} width="30" height="30" className="d-inline-block align-top" alt="Celtic Sea Dive Shop Logo" />
+                            Celtic Sea Dive Shop
+                        </a>
+
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarCollapse">
+                            <ul className="nav navbar-nav mr-auto">
+                                <li className="nav-item ">
+                                    <a className="nav-link" href="/">Home</a>
+                                </li>
+                                <li className="nav-item dropdown">
+
+                                    <a className="nav-link active dropdown-toggle" href="test.html" id="nav-dropdown" data-bs-toggle="dropdown"
+                                        aria-expanded="false" >
+                                        Courses
+                                    </a>
+
+                                    <ul className="dropdown-menu active" aria-labelledby="nav-dropdown">
+                                        <li><a className="dropdown-item" href="/OpenWater">Open Water</a></li>
+                                        <li><a className="dropdown-item active" href="/AdvOpenWater">Advanced Open Water</a></li>
+                                        <li><a className="dropdown-item" href="/">Rescue Diving</a></li>
+                                        <li>
+                                            <hr className="dropdown-divider" />
+                                        </li>
+                                        <li><a className="dropdown-item" href="/">All Dive Courses</a></li>
+                                    </ul>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="order.html">Dive Shop</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link " href="piesubscription.html">
+                                        Celtic subscription
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+            </header>
+            <main role="main">
+                <div className="container-fluid jumbotron jumbotron-detail-adv-open-water py-5">
+                    <div className="container">
+                        <h1 className="display-3 fw-bold text-white">Advanced Open Water Dive</h1>
+                    </div>
+                </div>
+
+                <div className="container">
+
+                    <h1 className="fw-bolder">Advanced Open Water Dive
+                        <small className="fw-normal fst-italic"> Step up with your Diving Skills!</small>
+                    </h1>
+
+                    <div className="row ">
+                        <div className="col-6 col-lg-8">
+                            <img src={advancedopen} className="img-fluid rounded-1" alt="advanced open water" />
+                        </div>
+
+                        <div className="col-6 col-lg-4">
+                            <h3 className="my-3">About Advanced Open Water Dive Course</h3>
+                            <p className="lead text-center">Gain more skills with Advanced Open Water Dive Course!</p>
+                            <p className="user-select-none">What every diver should know before they purchase any scuba equipment will be covered in this
+                                program. If you ever have experienced equipment problems in the water or have not felt totally
+                                comfortable on a dive, then this program is for you. The emphasis of this course is on local diving
+                                including proper thermal protection, weighting, and air control. These subjects are all covered in
+                                greater depth than your open water scuba class. The diving theory far exceeds other programs used
+                                for advanced diver certification. This program is specifically designed to teach divers the proper
+                                use of the different styles of scuba equipment — Jacket vs. Back Buoyancy, Integrated vs.
+                                Conventional Weight Systems, Steel vs. Aluminum Tanks, Vented vs. Paddle Fins, and Wet vs. Dry
+                                Suits.
+                            </p>
+                            <h3>This course includes the following :</h3>
+                            <ul>
+                                <li>PADI Advanced Open Water booklet</li>
+                                <li>15 Hours Classroom Sessions</li>
+                                <li>15 Hours Pool Training</li>
+                                <li>5 Ocean Dives</li>
+                            </ul>
+                            <p className="user-select-none">All equipment will be provided for
+                                the length of the course.</p>
+
+                            <div className="row">
+
+                                <div className="col-md-4">
+                                    <strong>$250.00</strong>
+                                </div>
+                                <div className="col-md-8">
+                                    <a type="button" href="shoppingbasket.html" className="btn btn-success float-end"
+                                        data-bs-toggle="offcanvas" data-bs-target="a.html" aria-controls="shoppingCart">
+                                        Add to cart
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-4 mb-3 mt-2 bg-light rounded border border-primary">
+                        <h4>A quote from Celtic Diving Team</h4>
+                        <p className="mb-0 fst-italic"> We dive not to escape life, but for life not to escape us.
+                        </p>
+                    </div>
+
+                    <h3 className="display-5 my-4">Other customers also tried...</h3>
+
+                    <div className="row">
+                        <div className="col-md-3 col-sm-6 mb-3">
+                            <a href="/">
+                                <img className="img-fluid" src={homedolphin} alt="" />
+                            </a>
+                            <p>Close encouter with Dolphin</p>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6 mb-3">
+                            <a href="/">
+                                <img className="img-fluid" src={mermaid} alt="" />
+                            </a>
+                            <p>Discover Mermaid</p>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6 mb-3">
+                            <a href="/">
+                                <img className="img-fluid" src={photography} alt="" />
+                            </a>
+                            <p>Underwater Photography</p>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6 mb-3">
+                            <a href="/">
+                                <img className="img-fluid" src={boatdiver} alt="" />
+                            </a>
+                            <p>Boat Diving</p>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </main>
+
+            <footer class="main-footer text-white text-center text-lg-start bg-primary">
+
+                <div className="container p-4">
+
+                    <div className="row mt-4">
+                        <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
+
+                            <img src={dolphin3} width="180" height="180" className="d-inline-block align-top"
+                                alt="Celtic Sea Dive Shop Logo" />
+
+                        </div>
+                        <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+
+                            <h5 className="text-uppercase mb-4">Follow Us</h5>
+
+
+                            <div className="mt-4">
+                                <a type="button" className="btn btn-floating btn-primary btn-lg"><i className="fab fa-facebook-f"></i></a>
+                                <a type="button" className="btn btn-floating btn-primary btn-lg"><i className="fab fa-instagram"></i></a>
+                                <a type="button" className="btn btn-floating btn-primary btn-lg"><i className="fab fa-twitter"></i></a>
+                                <a type="button" className="btn btn-floating btn-primary btn-lg"><i className="fab fa-google-plus-g"></i></a>
+                            </div>
+
+
+                            <ul className="fa-ul" >
+                                <li className="mb-3">
+                                    <span className="fa-li"><i className="fas fa-home"></i></span><span className="ms-1">Unit 1 Blackrock Road Kinsale Ireland</span>
+                                </li>
+                                <li className="mb-3">
+                                    <span className="fa-li"><i className="fas fa-envelope"></i></span><span className="ms-1">info@celticdiveshop.com</span>
+                                </li>
+                                <li className="mb-3">
+                                    <span className="fa-li"><i className="fas fa-phone"></i></span><span className="ms-1">+ 353 234 567 88</span>
+                                </li>
+                                <li className="mb-3">
+                                    <span className="fa-li"><i className="fas fa-print"></i></span><span className="ms-1">+ 353 234 567 89</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+                            <h5 className="text-uppercase mb-4">Opening hours</h5>
+
+                            <table className="table text-center text-white">
+                                <tbody className="font-weight-normal">
+                                    <tr>
+                                        <td>Mon - Thu:</td>
+                                        <td>8am - 9pm</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Fri - Sat:</td>
+                                        <td>8am - 5pm</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Sunday:</td>
+                                        <td>9am - 7pm</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div className="copyright text-center py-3">Copyright © 2022 Celtic Sea Dive Shop. All Rights Reserved.</div>
+
+
+            </footer>
+
+
+        </div >
+    );
+}
+
+export default AdvOpenWater;
